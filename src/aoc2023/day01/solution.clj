af000c9b7d86012b3ca720e478e6e92800f7054c
Sheng Yunzhou <shengyunzhou@gmail.com>
2023-12-02 14:55:40 +1100

    Add solution to day 1 part 2

--- src/aoc2023/day01/solution.clj
+++ src/aoc2023/day01/solution.clj
@@ -14,3 +14,32 @@
 (part-one "resources/day01/sample.txt")
 (part-one "resources/day01/input.txt")
 
+(defn clean-input [s]
+  (reduce (fn [acc [old new]]
+            (string/replace acc old new))
+          s
+          {"one" "one1one",
+           "two" "two2two",
+           "three" "three3three",
+           "four" "four4four",
+           "five" "five5five",
+           "six" "six6six",
+           "seven" "seven7seven",
+           "eight" "eight8eight",
+           "nine" "nine9nine"}))
+
+(clean-input "eightwothree")
+
+(defn part-two [input]
+  (->> input
+       slurp
+       (string/split-lines)
+       (map clean-input)
+       (map #(re-seq #"\d" %))
+       (map (juxt first last))
+       (map #(string/join %))
+       (map #(Integer/parseInt %))
+       (reduce +)))
+
+(part-two "resources/day01/sample_part2.txt")
+(part-two "resources/day01/input.txt")
