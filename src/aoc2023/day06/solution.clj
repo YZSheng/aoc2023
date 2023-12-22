8b121f7ecd2879e64bbc6899c70bb23cb1c250a6
Sheng Yunzhou <shengyunzhou@gmail.com>
2023-12-22 11:50:04 +0800

    Add part 1 day 06

---
+++ src/aoc2023/day06/solution.clj
@@ -0,0 +1,25 @@
+(ns aoc2023.day06.solution
+  (:require [clojure.string :as string]))
+
+(defn get-race-results [[t distance]]
+  (->> (for [hold-t (range 1 t)
+             :let [speed hold-t
+                   remaining-t (- t hold-t)
+                   travel-distance (* speed remaining-t)]]
+         [hold-t travel-distance])
+       (filter #(> (second %) distance))
+       count))
+
+(defn part-1 [input]
+  (->> input
+       slurp
+       string/split-lines
+       (map #(re-seq #"\d+" %))
+       (map (fn [nums-str]
+              (map parse-long nums-str)))
+       (apply map vector)
+       (map get-race-results)
+       (apply *)))
+
+(part-1 "resources/day06/sample.txt")
+(part-1 "resources/day06/input.txt")
