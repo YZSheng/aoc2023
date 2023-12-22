35259a987d70b9c3c15fdd509e33102552013447
Sheng Yunzhou <shengyunzhou@gmail.com>
2023-12-22 17:04:09 +0800

    Add part 2 day 08

--- src/aoc2023/day08/solution.clj
+++ src/aoc2023/day08/solution.clj
@@ -1,5 +1,6 @@
 (ns aoc2023.day08.solution
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.math.numeric-tower :as math]))
 
 (defn parse-to-map [network]
   (->> network
@@ -32,5 +33,36 @@
 (part-1 "resources/day08/sample_two.txt")
 (part-1 "resources/day08/input.txt")
 
-(comment
-  (nth (cycle "ab") 4))
+(defn get-command [commands n]
+  (let [l (count commands)
+        m (mod n l)]
+    (nth commands m)))
+
+(defn get-ghost-step-count [node commands map-input]
+  (loop [counter 0
+         node node]
+    (let [command (get-command commands counter)
+          [l r] (map-input node)
+          next-node (if (= \L command)
+                      l
+                      r)]
+      (if (str/ends-with? next-node "Z")
+        (inc counter)
+        (recur (inc counter) next-node)))))
+
+(defn lcm [numbers]
+  (reduce (fn [acc n]
+            (let [gcd (math/gcd acc n)]
+              (* acc (/ n gcd))))
+          1
+          numbers))
+
+(defn part-2 [input]
+  (let [[commands map-input] (parse-input input)
+        map-input (parse-to-map map-input)
+        start-nodes (filter #(str/ends-with? % "A") (keys map-input))]
+    (lcm (for [node start-nodes]
+      (get-ghost-step-count node commands map-input)))))
+
+(part-2 "resources/day08/sample_three.txt")
+(part-2 "resources/day08/input.txt")
