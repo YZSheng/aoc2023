0073bf1fe9edae6d2c2826122a314f5d5203b516
Sheng Yunzhou <shengyunzhou@gmail.com>
2023-12-22 15:13:09 +0800

    Add part 2 day 07

---
+++ src/aoc2023/day07/part2.clj
@@ -0,0 +1,78 @@
+(ns aoc2023.day07.part2
+  (:require [clojure.string :as str]))
+
+(def hand-strength {[5] 70000000
+                    [1 4] 60000000
+                    [2 3] 50000000
+                    [1 1 3] 40000000
+                    [1 2 2] 30000000
+                    [1 1 1 2] 20000000
+                    [1 1 1 1 1] 10000000})
+
+(def card-to-number {"2" 2
+                     "3" 3
+                     "4" 4
+                     "5" 5
+                     "6" 6
+                     "7" 7
+                     "8" 8
+                     "9" 9
+                     "T" 10
+                     "J" 1
+                     "Q" 12
+                     "K" 13
+                     "A" 14})
+
+(defn card-to-decimal [card-digits]
+  (reduce + (map (fn [digit index] (* digit (Math/pow 15 index)))
+                 (reverse card-digits)
+                 (range))))
+
+(defn calculate-cards-value [hand]
+  (->> hand
+       (#(str/split % #""))
+       (map card-to-number)
+       card-to-decimal))
+
+(defn calculate-hand-strength [hand]
+  (-> hand
+      (str/split #"")
+      frequencies
+      vals
+      sort
+      vec
+      hand-strength))
+
+(defn map-j [card]
+  (let [freq (frequencies (str/split card #""))]
+    (if (nil? (freq "J"))
+      card
+      (str/replace card "J" (or (->> freq
+                                     (filter #(not= "J" (first %)))
+                                     (sort-by (juxt val (comp card-to-number first)))
+                                     last
+                                     first) "A")))))
+
+(defn calculate-hand-score [hand]
+  (+ (calculate-hand-strength (map-j hand)) (calculate-cards-value hand)))
+
+(defn parse-input [input]
+  (->> input
+       slurp
+       str/split-lines
+       (map #(str/split % #"\s"))))
+
+(defn calculate-total-winnings [winnings]
+  (reduce + (map (fn [winning index]
+                   (* winning (inc index))) winnings (range))))
+
+(defn part-2 [input]
+  (->> input
+       parse-input
+       (sort-by (comp calculate-hand-score first))
+       (map last)
+       (map parse-long)
+       (calculate-total-winnings)))
+
+(part-2 "resources/day07/sample.txt")
+(part-2 "resources/day07/input.txt")
